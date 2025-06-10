54aa0c328657ccce08b783ed46fdff31e6f4b38c
aashaypai <paiaashay@gmail.com>
2025-06-09 23:05:11 -0700

    Added bin function to Ghost

--- src/rayven/ghost_data.py
+++ src/rayven/ghost_data.py
@@ -2,15 +2,29 @@ from astropy import units as u
 from dataclasses import dataclass, field
 from batoid import RayVector
 from typing import List
+
+from scipy.stats import binned_statistic_2d
 import numpy as np
+
+from .constants import LSSTCamConstants
+
 @dataclass
 class Ghost:
-
     name: str
     ray: RayVector
     x: List[float] = field(default_factory=list)
     y: List[float] = field(default_factory=list)
     flux: List[float] = field(default_factory=list)
+
+    def bin(self, bins):
+        binned_ghost, _, _, _ = binned_statistic_2d(self.x, self.y, 
+                                                    range=[[LSSTCamConstants.fp_min_x, 
+                                                            LSSTCamConstants.fp_max_x],
+                                                           [LSSTCamConstants.fp_min_y, 
+                                                            LSSTCamConstants.fp_max_y]], 
+                                                    values=self.flux, statistic='sum', bins=bins)
+        return np.flipud(binned_ghost.T)
+        
     
 @dataclass
 class GhostBundle:
